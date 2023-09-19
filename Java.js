e453979c16aa162f459afa79e4871b69f8622fdd
h-sanjay <waqas.naveed211@gmail.com>
2023-09-19 11:24:59 +0500

    javascript start

---
+++ Java.js
@@ -0,0 +1,9 @@
+console.clear();
+console.log(2+2*7-455-54121);
+console.warn("This is warning for you");
+console.error("There is an error");
+
+document.write("This is message on dom")
+
+window.alert("This is alert")
+window.confirm("Are you sure")
