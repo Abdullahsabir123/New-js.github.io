ee6516d52eca1662099580337bceb85054921626
h-sanjay <waqas.naveed211@gmail.com>
2023-09-19 12:53:48 +0500

    Formula practise

--- Java.js
+++ Java.js
@@ -1,12 +1,6 @@
-console.log("The addition of 2+3=" , 2+3);
-console.log("The subtraction of 55-22=" , 55-22);
-console.log("The multiplication of 23*55=" , 23*55);
-console.log("The division of 55/33=" , 55/33);
+// velocity=d/t
 
-document.write("<h1> The addition of 2+3=", 2+3, "<br> <hr> </h1>");
-document.write("<h2> The subtraction of 55-22=", 55-22, "<br> <hr> </h2>");
-document.write("<h3> The multiplication of 23*55=", 23*55, "<br> <hr> </h3>");
-document.write("<h4> The division of 55/33=", 55/33, "<br> <hr> </h4>");
+distance=prompt("Enter the distance");
+time=prompt("Enter your time");
 
-window.alert("Page not found");
-window.confirm("still not found")
+document.write("The velocity is=", distance / time, "m/s");
